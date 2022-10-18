13aa98636bdcd2f400df6ca45abbb32534c0b3ac
Polina Guseva (U-course) <pn_guseva@mail.ru>
2022-10-18 17:21:30 +0300

    Task-objects

--- src/Objects.js
+++ src/Objects.js
@@ -8,6 +8,14 @@
   Объект после манипуляций следует вернуть в качестве результата работы функции.
 */
 export function personUpdate(data) {
+    if (data.gender == 'female' && data.hasOwnProperty('age')) {
+        delete data.age;
+    } else {
+        if (data.gender == 'male' && !data.hasOwnProperty('income')) {
+            data.income = 100000;
+        }
+    }
+    return data;
 }
 
 /*
@@ -15,6 +23,11 @@ export function personUpdate(data) {
   Верните список названий этих полей в алфавитном порядке в виде массива строк.
 */
 export function objectFieldsList(obj1, obj2, obj3) {
+    return [
+        ...Object.keys(obj1),
+        ...Object.keys(obj2),
+        ...Object.keys(obj3),
+    ].sort();
 }
 
 /*
@@ -23,4 +36,12 @@ export function objectFieldsList(obj1, obj2, obj3) {
   Количество клонов - count.
 */
 export function objectClone(obj, count) {
+    let array = [];
+    for (let i = 0; i < count; i++) {
+        array.push({
+            id: i,
+            ...JSON.parse(JSON.stringify(obj)),
+        });
+    }
+    return array;
 }
